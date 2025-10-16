139ecc598a0f3541eea0382dd8a79eefb91bc436
JBelthoff <jb@johnbelthoff.com>
2025-10-16 13:57:31 -0400

    Add project files.

---
+++ Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace poker.net.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/cactus_kev.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace poker.net.Pages
+{
+    public class cactus_kevModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
